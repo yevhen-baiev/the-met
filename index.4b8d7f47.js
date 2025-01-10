e400c94423043727ea34a17583592715f71bb63e
Yevhen Baiev <baev838@gmail.com>
2025-01-10 11:34:09 +0200

    make build

---
+++ index.4b8d7f47.js
@@ -0,0 +1,2 @@
+var e;(e=document.querySelector(".contacts__form")).addEventListener("submit",function(t){t.preventDefault(),e.reset()});
+//# sourceMappingURL=index.4b8d7f47.js.map
---
+++ index.853f315c.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".contacts__form");e.addEventListener("submit",t=>{t.preventDefault(),e.reset()});
+//# sourceMappingURL=index.853f315c.js.map
